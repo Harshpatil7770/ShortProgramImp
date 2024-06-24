cbc3c72942e5451c809301df50c8e27b580d6f2d
Harshawardhan Patil <patil_ha@xoriant.com>
2024-06-24 21:17:13 +0530

    Marker Interface is a empty interface

---
+++ MarkerInterface/src/com/java/marker/ADMIN.java
@@ -0,0 +1,11 @@
+package com.java.marker;
+
+public interface ADMIN {
+ /**
+  * Marker interface - it has no filed and methods.
+  * It provide hints to jvm i.e we are going to provide some important feature.
+  * Serilizable, Clonable , Remote are the marker interfaces.
+  * Main purpose is We can use the marker interface for extra or separate logic
+  * purpose , like checking the instanceOf class or etc.
+  */
+}
